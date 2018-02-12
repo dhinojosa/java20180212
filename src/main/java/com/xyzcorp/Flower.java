29ea944dd907110d76ddec07a968e5e0aa5e581b
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2018-02-12 14:32:16 -0500

    Added some more to builder pattern

--- src/main/java/com/xyzcorp/Flower.java
+++ src/main/java/com/xyzcorp/Flower.java
@@ -4,13 +4,25 @@ public class Flower {
 
 
     private final String latinName;
+    private final int petalCount;
 
     public Flower(String latinName) {
         this.latinName = latinName;
+        this.petalCount = 0;
+    }
+
+    public Flower(String latinName, int petalCount) {
+        this.latinName = latinName;
+        this.petalCount = petalCount;
+    }
+
+    public int getPetalCount() {
+        return this.petalCount;
     }
 
     public static class FlowerBuilder {
         private String latinName;
+        private int petalCount = 0;
 
         public FlowerBuilder latinName(String latinName) {
             this.latinName = latinName;
@@ -18,7 +30,12 @@ public class Flower {
         }
 
         public Flower build() {
-            return new Flower(latinName);
+            return new Flower(latinName, petalCount);
+        }
+
+        public FlowerBuilder petals(int i) {
+            this.petalCount = i;
+            return this;
         }
     }
 
--- src/test/java/com/xyzcorp/FlowerTest.java
+++ src/test/java/com/xyzcorp/FlowerTest.java
@@ -13,4 +13,24 @@ public class FlowerTest {
                          .build();
         assertEquals("Narcissus", f.getLatinName());
     }
+
+    @Test
+    public void testFlowerBuilderWithPedalsAndLatinName() {
+        Flower f = Flower.builder()
+                         .latinName("Narcissus")
+                         .petals(7)
+                         .build();
+        assertEquals("Narcissus", f.getLatinName());
+        assertEquals(7, f.getPetalCount());
+    }
+
+    @Test
+    public void testFlowerBuilderWithPedalsAndLatinName2() {
+        Flower f = Flower.builder()
+                         .petals(7)
+                         .latinName("Narcissus")
+                         .build();
+        assertEquals("Narcissus", f.getLatinName());
+        assertEquals(7, f.getPetalCount());
+    }
 }
